9f2c15fa9ed9c9022973ee1b5638d589f382d8b6
Sam Sheets <sam.sheets94@gmail.com>
2021-04-12 11:04:32 -0600

    playing with npm and superhero and supervillain external modules

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
 
 //to open up REPL in the terminal, type 'node'. (ctrl + c twice to exit)
 
+//internal node.js module
 const fs = require('fs')
+//external node.js module
+const superheroes = require('superheroes')
+const supervillains = require('supervillains') 
+
 
 //create a copy of file1 to file2. To run in terminal, type 'node index.js'
 //'file1.txt' is the source text and 'file2.txt' is the destination text
-fs.copyFileSync('file1.txt', 'file2.txt')
+//fs.copyFileSync('file1.txt', 'file2.txt')
+
+let mySuperheroName = superheroes.random()
+console.log('superhero name: ', mySuperheroName)
+
+let mySupervillainName = supervillains.random()
+console.log('supervillain name: ', mySupervillainName)
